6876de5f86ad31a9ca3b5957d754307de02da732
elionardosantos <elionars@gmail.com>
2026-04-14 17:53:18 -0300

    Implementação inicial

---
+++ src/Interface/CalculoDesconto.java
@@ -0,0 +1,9 @@
+package Interface;
+
+public interface CalculoDesconto {
+
+    Double calcularINSS(Double salarioBruto);
+    Double calcularIR(Double salarioBruto);
+    Double calcularSalario();
+
+}
