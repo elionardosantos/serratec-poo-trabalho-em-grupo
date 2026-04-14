4aeaf44125f40932a50b7f0a2c7594215aed8d34
Bryanxrt <higorbraga24@gmail.com>
2026-04-14 16:52:46 -0300

    Adicionando UNUM

--- src/Enum/FaixasSalarioEnum.java
+++ src/Enum/FaixasSalarioEnum.java
@@ -3,11 +3,20 @@ package Enum;
 public enum FaixasSalarioEnum {
 
 //    FAIXA(REMUNERACAO, ALIQUOTA_POR_FAIXA, )
-    FAIXA1(1518.00);
+    FAIXA1(1518.00, 7.5),
+    FAIXA2(2793.88, 9),
+    FAIXA3(4190.83, 12),
+    FAIXA4(8157.41, 14);
 
-    Double salario;
+    private Double salario;
+    private Double aliquita;
 
-    FaixasSalarioEnum(Double salario) {
-        salario = salario;
+    FaixasSalarioEnum(Double salario, double aliquita) {
+        this.salario = salario;
+        this.aliquita = aliquita;
+    }
+
+    public Double getSalario() {
+        return salario;
     }
 }
